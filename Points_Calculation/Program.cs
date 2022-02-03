a041035bde43d0c92ff0ea2ce2327a708ccc95e3
Md Hasan <mdpabel15@gmail.com>
2022-02-03 14:58:08 -0500

    Add project files.

---
+++ Points_Calculation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points_Calculation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter amount");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            if (amount >= 50 && amount < 100)
+            {
+                 int points1= (amount - 50);
+                Console.WriteLine("Total points " + points1);
+            }
+            else if (amount >= 100)
+            {
+                 int points2 = (2 * (amount - 100) +50);
+                Console.WriteLine("Total points " + points2);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
